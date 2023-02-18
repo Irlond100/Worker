67e4b16b730ba8b1eadb9a87ad0aeb196a69435a
Irlond100 <121184697+Irlond100@users.noreply.github.com>
2023-02-18 19:01:54 +0300

    18.02.2023

---
+++ Worker.java
@@ -0,0 +1,38 @@
+package Worker;
+
+public class Worker {
+	
+	private OnTaskDoneListener callback;
+	private OnTaskErrorListener errorCallback;
+	
+	public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+		this.errorCallback = errorCallback;
+		this.callback = callback;
+	}
+	
+	public void start() {
+		for (int i = 0; i < 100; i++) {
+			if (i <= 66) {
+				callback.onDone("Task " + i + " is done");
+			} else {
+				errorCallback.onError("Task " + i + " is error");
+			}
+			
+		}
+	}
+	
+	@FunctionalInterface
+	public interface OnTaskDoneListener {
+		
+		void onDone(String result);
+		
+	}
+	
+	@FunctionalInterface
+	public interface OnTaskErrorListener {
+		
+		void onError(String result);
+		
+	}
+	
+}
